1eacec8e5e3a398d228f1e950f260c77e5117c96
AlekseySamokhin <lel.dude01@gmail.com>
2022-06-08 22:19:05 +0300

    test commit

---
+++ main.js
@@ -0,0 +1,12 @@
+async function range(start, end) {
+    for(let j = start; j<= end; i++) {
+        yield Promise.resolve(j)
+}
+}
+
+(async () => {
+    const gen = range(1, 3)
+    for await (const item of gen) {
+        console.log(item)
+    }
+})()
